81fc14168534cc12499fd5ea46a41e3d70943299
sonaw <sonaw@host.docker.internal>
2023-07-24 08:45:04 +0100

    Second change

--- FirstProject/src/fileOperations/ReadUsingScanner.java
+++ FirstProject/src/fileOperations/ReadUsingScanner.java
@@ -2,7 +2,7 @@ package fileOperations;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+//import java.io.FileReader;
 import java.util.Scanner;
 
 public class ReadUsingScanner
